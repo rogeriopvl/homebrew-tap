617ab7f3f9f99feafadf5856ea1e282f48be5c7d
rogeriopvl <rogerio@hey.com>
2025-12-20 19:06:32 +0000

    chore(fizzy-cli): update to v0.2.1

--- Formula/fizzy-cli.rb
+++ Formula/fizzy-cli.rb
@@ -1,8 +1,8 @@
 class FizzyCli < Formula
   desc "CLI for https://fizzy.do"
   homepage "https://github.com/rogeriopvl/fizzy-cli"
-  url "https://github.com/rogeriopvl/fizzy-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9a38f1fb05bb875f41e15f0a55d3b4fab6001a3984394e22838f727371234c96"
+  url "https://github.com/rogeriopvl/fizzy-cli/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "4d7e3c7359dfbf8b821c9fd5bb9b7050772ed0258464264d7e4ce691f4548ffa"
   license "MIT"
   head "https://github.com/rogeriopvl/fizzy-cli.git", branch: "main"
 
